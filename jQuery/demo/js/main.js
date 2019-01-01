8a1ee796d1649ec655afecb3244d35b1f6468137
kuibu <chen_yff@126.com>
2019-01-01 21:42:48 +0800

    test
    hahaha

---
+++ jQuery/demo/js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+    /*选中页面中所有的input[data-rule]*/
+  var $inputs = $('[data-rule]')
+     ,$form = $('#signup')
+     ,inputs = []
+  $inputs.each(function(index,node){
+    var tmp = new Input(node);
+    inputs.push(tmp);
+  });
+  $form.on('submit',function(e){
+      e.preventDefault();
+      $inputs.trigger('blur');
+      for(var i=0;i<inputs.length;i++){
+          var item = inputs[i];
+          var r = item.validator.is_valid();
+          if(!r){
+              return;
+          }
+      }
+      alert('success');
+  })
+    
+    
+})
---
+++ jQuery/demo/js/validator.js
@@ -0,0 +1,66 @@
+$(function(){
+    window.Validator = function(val,rule){
+        this.is_valid = function(new_val){
+            var key;
+            if(new_val !== undefined){
+                val = new_val;
+            }
+            
+            if(!rule.required && val){
+                return true;
+            }
+            for(key in rule){
+                if(key === "required"){
+                    continue;
+                }
+                var r = this['validate_'+key]();
+                if(!r){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        this.validate_max = function(){
+            pre_max_min();
+            return val<=rule.max;
+        }
+        this.validate_min = function () {
+            pre_max_min();
+            return val >= rule.min;
+        }
+        this.validate_maxlength = function () {
+            pre_length();
+            return val.length <= rule.maxlength;
+        }
+        this.validate_minlength = function () {
+            pre_length();
+            return val.length >= rule.minlength;
+        }
+        this.validate_numeric = function(){
+            return $.isNumeric(val);
+        }
+        this.validate_required = function(){
+            var real = $.trim(val);
+            if(!real && real !== 0){
+                return false;
+            }
+            return true;
+        }
+        this.validate_pattern = function(){
+            var reg = new RegExp(rule.pattern);
+            return reg.test(val);
+        }
+
+
+        /*用于完成this.validate_max或this.validate.min的前置工作*/
+        function pre_max_min(){
+            val = parseFloat(val);
+        }
+        /*用于完成this.validate_maxlength或this.validate.minlength的前置工作*/
+        function pre_length() {
+            val = val.toString();
+        }
+    }
+})
